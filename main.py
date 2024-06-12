7f8b1d5a2830fea4d87cf16b5723f18a41666a9c
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-06-12 14:15:30 +0100

    Classes and class methods

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-# name = input('What is your name: ')
+class Product():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-# base_salary = float(input('What is your annual salary: '))
+    def __str__(self):
+        return f"Product: {self.name} | Price: £{self.price}"
+    
+    def discount(self, percentage):
+        self.price = self.price - (self.price * percentage / 100)
+        return f"Discounted price: £{self.price}"
+    
+    def discount_value(self, value):
+        value = value / 100
+        return f"Discount value is: {value} OR {value * 100}%"
+        
+        
+item = Product("Phone", 75.50)
 
-# year_of_service = int(input('How many years have you worked: '))
+print(item)
 
-# if year_of_service > 3:
-#     bonus_salary = round(base_salary * 0.07, 2)
-#     print(f'Hello {name}, your bonus for {year_of_service} years of service is £{bonus_salary:.2f}.')
-# else:
-#     bonus_salary = 0
-#     print(f'Hello {name}, your bonus for {year_of_service} years of service is £{bonus_salary:.2f}.')
-
-# dummy = ('Tekkem', 'Kem', 'Kemmy', 'KemmyTekkem')
-# number_set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 4, 10}
-
-# thisset = {"apple", "banana", "cherry", True, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 4,}
-
-# # dummy[1] = 'Keommy'
-
-# dummy_list = list(dummy)
-
-# print('Tuple:', dummy)
-# print('List', dummy_list)
-# print('Set:', number_set)
-# print('Set:', thisset)
-
-userInput1 = input('Enter a list item, any text: ')
-userInput2 = input('Enter another item, any text: ')
-userInput3 = input('Enter a final list item, any text: ')
-
-# dataOutput = list(userInput1, userInput2, userInput3)
-dataOutput = [userInput1, userInput2, userInput3]
-dataOutput.append('Sushi')
-print(dataOutput)
-print(type(dataOutput))
+print(item.discount(20))
+print('---------- Discount Value ----------')
+print(item.discount_value(20))
